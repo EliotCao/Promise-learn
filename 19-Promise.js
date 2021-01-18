d6feeef2b5930862354122ca2d0fd7d0dc0b19cc
Eliot <484900039@qq.com>
2021-01-18 15:53:20 +0800

    自定义Promise resolve和reject实现

--- 19-Promise.js
+++ 19-Promise.js
@@ -1,9 +1,23 @@
 function Promise(executor){
+    this.PromistState = 'pending';
+    this.PromistResult = null;
+    //保存实例对象的this的值
+    const self = this;
 
     //resolve函数
-    function resolve(data){}
+    function resolve(data){
+        //1.修改对象的状态（promiseState）
+        self.PromistState = 'fulfilled';
+        //2.设置对象结果值（promiseResult）
+        self.PromistResult = data;
+    }
     //reject函数
-    function reject(data){}
+    function reject(data){
+        //1.修改对象的状态（promiseState）
+        self.PromistState = 'rejected';
+        //2.设置对象结果值（promiseResult）
+        self.PromistResult = data;
+    }
 
     //同步调用【执行器方法】
     executor(resolve,reject);
