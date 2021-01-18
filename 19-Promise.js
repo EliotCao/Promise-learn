68008519ffc430c52320105529fb6c7f3369b1a0
Eliot <484900039@qq.com>
2021-01-18 15:47:08 +0800

    自定义Promise resolve和reject

--- 19-Promise.js
+++ 19-Promise.js
@@ -1,5 +1,12 @@
 function Promise(executor){
 
+    //resolve函数
+    function resolve(data){}
+    //reject函数
+    function reject(data){}
+
+    //同步调用【执行器方法】
+    executor(resolve,reject);
 }
 
 //添加then方法
